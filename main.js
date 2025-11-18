76750b43759c7eaa9b871c7b86a670bde0e4c85b
William Matton <williammatton@gmail.com>
2025-11-18 13:58:30 +0100

    Remove api.js and place the API-call functions in main.js instead.

--- main.js
+++ main.js
@@ -25,6 +25,37 @@ function loadChallengesPage() {
     window.location.href = 'OurChallenges.html';
 }
 
+/* --------------------- API FUNCTIONS --------------------- */
+async function getChallenges() {
+    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+    const data = await res.json();
+    return data;
+}
+
+async function getAvailableTimes(challengeID) {
+    const date = document.querySelector("#date").value;
+    const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=' + date + '&challenge' + challengeID);
+    const data = await res.json();
+    return data;
+}
+
+async function postBooking(data) {
+    await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            challenge: data.challengeID,
+            name: data.customerName,
+            email: data.customerEmail,
+            date: data.bookedDate,
+            time: data.bookedTime,
+            participants: data.participants
+        })
+    });
+}
+
 /* --------------------- Handle Filter Challenges ------------------------- */
 document.addEventListener('DOMContentLoaded', function () {
     const filterBtn = document.querySelector('.filterBtn');
